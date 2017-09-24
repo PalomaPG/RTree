0c40cf3db6c14c1e0ba538391e4ad9a38254ec6d
Sebastián Pujá López <sebastian.puja.lopez@gmail.com>
2017-09-23 22:38:35 -0300

    Adds Coord2D, MBR, NodeEntry and Node classes with tests. Implementation of Node is incomplete yet.

--- jRtree/src/main/jRtree/MBR.java
+++ jRtree/src/main/jRtree/MBR.java
@@ -1,4 +1,4 @@
-// MBR implementation. Assumes 2D rectangles
+// MBR implementation. Assumes 2D rectangles, doesn't make any check.
 
 public class MBR {
 
@@ -8,24 +8,16 @@ public class MBR {
     private Coord2D pt4;
 
     public MBR(double[][] mbr_square) {
-        this.pt1 = new Coord2D(mbr_square[0]);
-        this.pt2 = new Coord2D(mbr_square[1]);
-        this.pt3 = new Coord2D(mbr_square[2]);
-        this.pt4 = new Coord2D(mbr_square[3]);
+        this(new Coord2D(mbr_square[0]), new Coord2D(mbr_square[1]), new Coord2D(mbr_square[2]),
+                new Coord2D(mbr_square[3]));
     }
 
     public MBR(double[] pt1, double[] pt2, double[] pt3, double[] pt4) {
-        this.pt1 = new Coord2D(pt1);
-        this.pt2 = new Coord2D(pt2);
-        this.pt3 = new Coord2D(pt3);
-        this.pt4 = new Coord2D(pt4);
+        this(new Coord2D(pt1), new Coord2D(pt2), new Coord2D(pt3), new Coord2D(pt4));
     }
 
     public MBR(Coord2D[] coord2DS){
-        this.pt1 = coord2DS[0];
-        this.pt2 = coord2DS[1];
-        this.pt3 = coord2DS[2];
-        this.pt4 = coord2DS[3];
+        this(coord2DS[0], coord2DS[1], coord2DS[2], coord2DS[3]);
     }
 
     public MBR(Coord2D pt1, Coord2D pt2, Coord2D pt3, Coord2D pt4){
@@ -54,11 +46,11 @@ public class MBR {
     @Override
     public boolean equals(Object aMBR){
         if (aMBR instanceof MBR){
-            MBR castMBR = (MBR) aMBR;
-            return this.pt1.equals(castMBR.getPt1()) &&
-                    this.pt2.equals(castMBR.getPt2()) &&
-                    this.pt3.equals(castMBR.getPt3()) &&
-                    this.pt4.equals(castMBR.getPt4());
+            MBR m = (MBR) aMBR;
+            return this.pt1.equals(m.getPt1()) &&
+                    this.pt2.equals(m.getPt2()) &&
+                    this.pt3.equals(m.getPt3()) &&
+                    this.pt4.equals(m.getPt4());
         }
         else {
             return false;
