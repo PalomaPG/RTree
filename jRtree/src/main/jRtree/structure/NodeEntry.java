620944646228954d45a287ada097a61965968b4a
Palomapg <paloma.perez.gar@gmail.com>
2017-09-29 07:33:42 -0300

    New NodeEntry test in progress

--- jRtree/src/main/jRtree/structure/NodeEntry.java
+++ jRtree/src/main/jRtree/structure/NodeEntry.java
@@ -21,4 +21,29 @@ public class NodeEntry{
     public INode getChild(){
         return child;
     }
+
+    public double calculateEnlargement(NodeEntry ne){
+
+        //Coordenandas del MBR de este NodeEntry
+
+        MBR ne_mbr = ne.getMBR();
+
+
+        /*
+        Caso en que el MBR no crezca nada: esta entrada cabe completamente
+        en el.
+         */
+        if(mbr.min_y()>=ne_mbr.min_y() && mbr.max_y()<= ne_mbr.max_y() &&
+                mbr.min_x()>=ne_mbr.min_x() && mbr.max_x()<=ne_mbr.max_x()) {
+            System.err.println("within MBR");
+            return 0.0;
+        }
+        /*Caso en el que este MBR posea alguna coordenada mayor o menor a las
+        * que definen el rango del MBR contenedor */
+        else{
+
+            return -1;
+        }
+
+    }
 }
